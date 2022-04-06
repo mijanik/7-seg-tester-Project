305e3b60c453b77112855c976db864bc1c0d4957
Miłosz Janik <mijanik@student.agh.edu.pl>
2022-04-06 19:15:21 +0200

    Add files via upload

---
+++ 7seg_tester_project/Objects/buttons.d
@@ -0,0 +1,10 @@
+.\objects\buttons.o: buttons.c
+.\objects\buttons.o: C:\Users\anton\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
+.\objects\buttons.o: C:\Users\anton\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\buttons.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\buttons.o: C:\Users\anton\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\buttons.o: C:\Users\anton\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\buttons.o: C:\Users\anton\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\buttons.o: C:\Users\anton\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
+.\objects\buttons.o: buttons.h
+.\objects\buttons.o: fsm.h
---
+++ 7seg_tester_project/buttons.h
@@ -0,0 +1,9 @@
+#ifndef buttons_h
+#define buttons_h
+#include "MKL05Z4.h"
+
+void buttonsInitialize(void);
+void set_flag_button_pressed(void);
+void fsmInitialize1(void);
+#endif
+
